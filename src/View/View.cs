71b1a9cab65523a5af0c4c73c23ee4479eaee6ae
frostypls <forsyth.daniel@gmail.com>
2016-10-02 23:23:53 +1100

    Added View controller

---
+++ src/View/View.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using SwinGameSDK;
+using static SwinGameSDK.SwinGame;
+
+namespace PantMerchant 
+{
+    /// <summary>
+    /// Global class used to manage drawing to 
+    /// the screen.
+    /// </summary>
+    static class View
+    {
+        /// <summary>
+        /// Used to draw all IDrawable objects to the screen.
+        /// </summary>
+        public static void Draw()
+        {
+            foreach (IDrawable d in Registry.IDrawableList)
+            {
+                d.Draw();
+            }
+        }
+    }
+}

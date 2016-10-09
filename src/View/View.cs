00bdad601008f61abba6ef57f245ec9b8cad4d44
frostypls <forsyth.daniel@gmail.com>
2016-10-10 06:56:59 +1100

    Moved graphical operations to View class

--- src/View/View.cs
+++ src/View/View.cs
@@ -11,15 +11,28 @@ namespace PantMerchant
     /// </summary>
     static class View
     {
+        static View()
+        {
+            //Open the game window
+            SwinGame.OpenGraphicsWindow("PantMerchant", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen();
+        }
         /// <summary>
         /// Used to draw all IDrawable objects to the screen.
         /// </summary>
         public static void Draw()
         {
+            //Clear the screen and draw the framerate
+            SwinGame.ClearScreen(Color.White);
+            SwinGame.DrawFramerate(0, 0);
+
             foreach (IDrawable d in Registry.IDrawableList)
             {
                 d.Draw();
             }
+
+            //Draw onto the screen
+            SwinGame.RefreshScreen(60);
         }
     }
 }

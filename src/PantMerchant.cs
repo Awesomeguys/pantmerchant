a4f8a20d70f72efb5af3edf9560ff6e14ae9b575
frostypls <forsyth.daniel@gmail.com>
2016-10-10 06:57:19 +1100

    Added test UI

--- src/PantMerchant.cs
+++ src/PantMerchant.cs
@@ -2,6 +2,8 @@ using System;
 using SwinGameSDK;
 using static SwinGameSDK.SwinGame;
 
+using System.Collections.Generic;
+
 namespace PantMerchant
 {
     public class PantMerchant
@@ -13,7 +15,7 @@ namespace PantMerchant
         ///<returns>Boolean indicating whether the user has requested to close the program.</returns>
         static bool EndProgramRequested(){
             bool endGame = false;
-            if  (    SwinGame.WindowCloseRequested()
+            if  (   SwinGame.WindowCloseRequested()
                 ||  SwinGame.KeyTyped(KeyCode.EscapeKey)) {
                 endGame = true;
             }
@@ -21,19 +23,42 @@ namespace PantMerchant
         }
 
         public static void Main()
-        {            
+        {
+
+            // Create some UIs
+            List<UIElement> MenuList = new List<UIElement>();
+            MenuList.Add(
+                new MenuElement(
+                    "Test 1",
+                    (Action)delegate { Console.WriteLine("Test 1"); },
+                    "Test 1",
+                    new Point2D() { X = 10, Y = 10 },
+                    new Point2D() { X = 50, Y = 10 }
+                )
+            );
+
+            MenuList.Add(
+                new MenuElement(
+                    "Test 2",
+                    (Action)delegate { Console.WriteLine("Test 2"); },
+                    "Test 2",
+                    new Point2D() { X = 10, Y = 10 },
+                    new Point2D() { X = 50, Y = 10 }
+                )
+            );
+
+            UIContainer Menu = new UIContainer(
+                    MenuList, "Test Menu", new Point2D() { X = 200, Y = 200 }, new Point2D() { X = 200, Y = 200 }
+            );
+
             //Run the game loop
-            while(!EndProgramRequested())
+            while (!EndProgramRequested())
             {
                 //Fetch the next batch of UI interaction
                 SwinGame.ProcessEvents();
-                
-                //Clear the screen and draw the framerate
-                SwinGame.ClearScreen(Color.White);
-                SwinGame.DrawFramerate(0,0);
-                
-                //Draw onto the screen
-                SwinGame.RefreshScreen(60);
+
+                // Draw stuff on the screen
+                View.Draw();
             }
         }
     }

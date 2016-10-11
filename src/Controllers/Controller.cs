fdf073ba01d669b80384d87573e93360b7e7a3e4
frostypls <forsyth.daniel@gmail.com>
2016-10-12 02:36:08 +1100

    Added controller classes to handle click actions

---
+++ src/Controllers/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SwinGameSDK;
+using static SwinGameSDK.SwinGame;
+
+namespace PantMerchant.Controllers
+{
+    abstract class Controller
+    {
+        public void DoClickActions()
+        {
+            foreach (IClickable c in Registry.IClickableList)
+            {
+                if (c.IsClicked())
+                {
+                    c.ClickAction();
+                }
+            }
+        }
+    }
+}
---
+++ src/Controllers/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SwinGameSDK;
+using static SwinGameSDK.SwinGame;
+
+namespace PantMerchant.Controllers
+{
+    class MainMenuController : Controller
+    {
+        private static MainMenuController _instance;
+        public static MainMenuController Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MainMenuController();
+                }
+                return _instance;
+            }
+        }        
+
+        private MainMenuController()
+        {
+
+        }
+
+        static MainMenuController()
+        {
+
+        }
+    }
+}

1213a592c5d9ca95722b95b2a4552dbd2a2431cf
frostypls <forsyth.daniel@gmail.com>
2016-10-12 00:10:20 +1100

    Removed extra vector class, cleaned up Point2D members

--- src/Vector.cs
+++ src/Vector.cs
@@ -27,38 +27,20 @@ namespace PantMerchant
 		// Overloaded operators for vector arithmetic
 
 		public static bool operator ==(Point2D p1, Point2D p2) {
-			if ((p1.X == p2.X) && (p1.Y == p2.Y)){
-				return true;
-			} else {
-				return false;
-			}
-		}
+            return (p1.X == p2.X) && (p1.Y == p2.Y);
+        }
 
 		public static bool operator !=(Point2D p1, Point2D p2) {
-			if ((p1.X == p2.X) && (p1.Y == p2.Y)){
-				return false;
-			} else {
-				return true;
-			}
-		}
+            return !((p1.X == p2.X) && (p1.Y == p2.Y));
+
+        }
 
 		public static Point2D operator +(Point2D p1, Point2D p2) {
 			return new Point2D(p1.X + p2.X, p1.Y + p2.Y);
 		}
 
-		public static Point2D operator +(Point2D p, Vector v) {
-			Point2D result = new Point2D();
-			result.X = p.X + (float)v.X;
-			result.Y = p.Y + (float)v.Y;
-			return result;
-		}
-
 		public static Point2D operator -(Point2D p1, Point2D p2) {
 			return new Point2D(p1.X - p2.X, p1.Y - p2.Y);
-		}
-
-		public static Point2D operator -(Point2D p, Vector v) {
-			return (Vector)p - v;
 		}
 
 		public static Point2D operator *(Point2D p, float i) {
@@ -81,104 +63,12 @@ namespace PantMerchant
 			return result;
 		}
 
-		public static implicit operator Point2D(SwinGameSDK.Point2D p){
+		public static implicit operator PantMerchant.Point2D(SwinGameSDK.Point2D p){
             // Automatically convert between my Point2D and SwinGame's Point2D
             PantMerchant.Point2D result = new PantMerchant.Point2D ();
 			result.X = p.X;
 			result.Y = p.Y;
 			return result;
-		}
-
-		public static implicit operator Vector(Point2D p){
-			// Automatically convert between my Point2D and Vector
-			return new Vector((double)p.X, (double)p.Y);
-		}
-	}
-
-	public class Vector {
-
-		public double X, Y;
-
-		public double Mag {
-			get { return Math.Sqrt((X * X) + (Y * Y)); }
-		}
-
-		public Vector UnitVector {
-			get {
-				if (Mag == 0) {
-					return new Vector (0, 0);
-				}
-				return new Vector (X / Mag, Y / Mag);
-			}
-		}
-
-		public Vector() : this(0, 0) {}
-		public Vector(double X, double Y) {
-			this.X = X;
-			this.Y = Y;
-		}
-
-		public override string ToString() {
-			return String.Format("{0}, {1}", X, Y);
-		}
-
-		public Vector GetRightAngle(){
-			return new Vector (this.X, -this.Y);
-		}
-
-		// Overloaded operators for vector arithmetic
-
-		public static bool operator ==(Vector v1, Vector v2) {
-			if ((v1.X == v2.X) && (v1.Y == v2.Y)){
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		public static bool operator !=(Vector v1, Vector v2) {
-			if ((v1.X == v2.X) && (v1.Y == v2.Y)){
-				return false;
-			} else {
-				return true;
-			}
-		}
-
-		public static Vector operator +(Vector v1, Vector v2) {
-			return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-		}
-
-		public static Vector operator -(Vector v1, Vector v2) {
-			return new Vector(v1.X - v2.X, v1.Y - v2.Y);
-		}
-
-		public static Vector operator *(int i, Vector v) {
-			return new Vector(v.X * i, v.Y * i);
-		}
-
-		public static Vector operator *(Vector v, int i) {
-			return i * v;
-		}
-
-		public static Vector operator *(double i, Vector v) {
-			return new Vector(v.X *(double)i, v.Y*(double)i);
-		}
-
-		public static Vector operator *(Vector v, double i) {
-			return new Vector(v.X *(double)i, v.Y*(double)i);
-		}
-
-		public static Vector operator /(Vector v, double d) {
-			return new Vector(v.X / d, v.Y / d);
-		}
-
-		public static Vector operator /(Vector v, int i) {
-			return new Vector(v.X / i, v.Y / i);
-		}
-
-		public static implicit operator Point2D(Vector v){
-			// Automatically convert between my Point2D and Vector
-			return new Point2D((float)v.X, (float)v.Y);
 		}
 	}
 }

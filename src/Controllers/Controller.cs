947e8406673e2db3840b2efb47a706d845e033c0
Daniel <forsyth.daniel@gmail.com>
2016-10-12 11:54:30 +1100

    Added Instance property to base controller class

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -6,6 +6,7 @@ namespace PantMerchant.Controllers
 {
     abstract class Controller
     {
+        public static Controller Instance { get; }
         public void DoClickActions()
         {
             foreach (IClickable c in Registry.IClickableList)

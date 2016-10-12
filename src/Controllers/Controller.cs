f7b3759f43d4ce418db82009ed5cfccedc88eaf4
Daniel <forsyth.daniel@gmail.com>
2016-10-12 17:42:54 +1100

    Moved registration into respective controller classe. Added method to perform controller actions

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using SwinGameSDK;
 using static SwinGameSDK.SwinGame;
+using System.Collections.Generic;
 
 namespace PantMerchant.Controllers
 {
     abstract class Controller
     {
+        public List<UIElement> UIElementList { get; }
+        public List<IClickable> IClickableList { get; }
+        public List<IDrawable> IDrawableList { get; }
         public static Controller Instance { get; }
+
+        protected Controller()
+        {
+            UIElementList = new List<UIElement>();
+            IClickableList = new List<IClickable>();
+            IDrawableList = new List<IDrawable>();
+        }
         public void DoClickActions()
         {
-            foreach (IClickable c in Registry.IClickableList)
+            foreach (IClickable c in StateController.IClickableList)
             {
                 if (c.IsClicked())
                 {
@@ -17,5 +28,12 @@ namespace PantMerchant.Controllers
                 }
             }
         }
+
+        /// <summary>
+        /// Overridden by child class. Allows controller actions to be 
+        /// performed regardless of which controller is currently in 
+        /// control.
+        /// </summary>
+        public abstract void DoControllerStuff();
     }
 }

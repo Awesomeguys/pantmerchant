991017e8b81ccc434ff73aaf61ca695a2879cf1d
frostypls <forsyth.daniel@gmail.com>
2016-10-16 00:58:52 +1100

    Rename from Pos to Coordinate

--- UnitTests/src/Entities/GridCell.cs
+++ UnitTests/src/Entities/GridCell.cs
@@ -18,14 +18,14 @@ namespace PantMerchantTests
         public void GetOriginCell()
         {
             // Position is correct
-            Assert.IsTrue(GameController.Instance.OriginGridCell.Pos == PantMerchant.Point2D.Origin);
+            Assert.IsTrue(GameController.Instance.OriginGridCell.Coordinate == PantMerchant.Point2D.Origin);
             // Grid reference is correct
             Assert.IsTrue(GameController.Instance.OriginGridCell == GameController.Instance.GetGrid(PantMerchant.Point2D.Origin));
         }
         [Test]
         public void NeighbourTop() {
             // Position is correct
-            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourTop.Pos == GameController.Instance.OriginGridCell.Pos + new PantMerchant.Point2D(0, 1));
+            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourTop.Coordinate == GameController.Instance.OriginGridCell.Coordinate + new PantMerchant.Point2D(0, 1));
 
             // Grid reference is correct
             Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourTop == GameController.Instance.GetGrid(PantMerchant.Point2D.Origin + new PantMerchant.Point2D(0, 1)));
@@ -35,7 +35,7 @@ namespace PantMerchantTests
         public void NeighbourRight()
         {
             // Position is correct
-            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourRight.Pos == GameController.Instance.OriginGridCell.Pos + new PantMerchant.Point2D(1, 0));
+            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourRight.Coordinate == GameController.Instance.OriginGridCell.Coordinate + new PantMerchant.Point2D(1, 0));
 
             // Grid reference is correct
             Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourRight == GameController.Instance.GetGrid(PantMerchant.Point2D.Origin + new PantMerchant.Point2D(1, 0)));
@@ -44,7 +44,7 @@ namespace PantMerchantTests
         [Test]
         public void NeighbourBottom()
         {
-            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourBottom.Pos == GameController.Instance.OriginGridCell.Pos + new PantMerchant.Point2D(0, -1));
+            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourBottom.Coordinate == GameController.Instance.OriginGridCell.Coordinate + new PantMerchant.Point2D(0, -1));
 
             // Grid reference is correct
             Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourBottom == GameController.Instance.GetGrid(PantMerchant.Point2D.Origin + new PantMerchant.Point2D(0, -1)));
@@ -53,7 +53,7 @@ namespace PantMerchantTests
         [Test]
         public void NeighbourLeft()
         {
-            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourLeft.Pos == GameController.Instance.OriginGridCell.Pos + new PantMerchant.Point2D(-1, 0));
+            Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourLeft.Coordinate == GameController.Instance.OriginGridCell.Coordinate + new PantMerchant.Point2D(-1, 0));
 
             // Grid reference is correct
             Assert.IsTrue(GameController.Instance.OriginGridCell.NeighbourLeft == GameController.Instance.GetGrid(PantMerchant.Point2D.Origin + new PantMerchant.Point2D(-1, 0)));

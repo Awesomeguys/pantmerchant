f6c94cbcdf698e5ae722c1adb3c6058f7dcbcbf5
Daniel <forsyth.daniel@gmail.com>
2016-10-15 14:59:35 +1100

    Added documentation and optimised overloads

--- src/Point2D.cs
+++ src/Point2D.cs
@@ -25,6 +25,13 @@ namespace PantMerchant
             return "[" + X + "," + Y + "]";
         }
 
+        /// <summary>
+        /// Returns the displacement of the provided Point2D from the 
+        /// current one. How many X units and how many Y units to travel 
+        /// to go from this one to the provided one.
+        /// </summary>
+        /// <param name="p">Point2D to compare with this one.</param>
+        /// <returns>Point2D representing the displacement between the provided one and the current one.</returns>
         public Point2D GetRelativePosition(Point2D p){
             return p - this;
         }
@@ -72,40 +79,66 @@ namespace PantMerchant
             return sum.GetHashCode();
         }
 
+        /// <summary>
+        /// Performs vector addition on two Point2Ds
+        /// </summary>
+        /// <param name="p1">Point2D to sum</param>
+        /// <param name="p2">Point2D to sum</param>
+        /// <returns>Returns the vector sum of p1 and p2</returns>
         public static Point2D operator +(Point2D p1, Point2D p2) {
 			return new Point2D(p1.X + p2.X, p1.Y + p2.Y);
 		}
 
+        /// <summary>
+        /// Performs vector subtraction on two Point2Ds
+        /// </summary>
+        /// <param name="p1">Point2D to perform subtraction on</param>
+        /// <param name="p2">Point2D to subtract from p1</param>
+        /// <returns>Returns the vector sum of p1 and -(p2)</returns>
 		public static Point2D operator -(Point2D p1, Point2D p2) {
 			return new Point2D(p1.X - p2.X, p1.Y - p2.Y);
 		}
 
-		public static Point2D operator *(Point2D p, float i) {
-			return new Point2D(Point2D.Round(p.X *i), Point2D.Round(p.Y*i));
+        /// <summary>
+        /// Performs scalar multiplication on a vector p with d
+        /// </summary>
+        /// <param name="p">The vector to multiply</param>
+        /// <param name="d">The scalar to multiply the p by</param>
+        /// <returns>The product of p and d</returns>
+		public static Point2D operator *(Point2D p, double d) {
+			return new Point2D(Point2D.Round(p.X *d), Point2D.Round(p.Y*d));
 		}
 
-		public static Point2D operator /(Point2D p, int i) {
-			return new Point2D(p.X / i, p.Y / i);
-		}
-
+        /// <summary>
+        /// Performs scalar division on a vector p with i, 
+        /// rounding the components of p to the nearest integer.
+        /// Equivalent to multiplying p by the reciprocal of i
+        /// </summary>
+        /// <param name="p">The vector to perform the division on</param>
+        /// <param name="d">The scalar to divide p by</param>
+        /// <returns>The quotient of p by d</returns>
 		public static Point2D operator /(Point2D p, double d) {
-			return new Point2D((Point2D.Round(p.X / d)), (Point2D.Round(p.Y / d)));
+            return p * (1 / d);
 		}
-
+        
+        /// <summary>
+        /// Performs implicit conversion between PantMerchant's Point2D and 
+        /// SwinGame's in-built Point2D class.
+        /// </summary>
+        /// <param name="p">PantMerchant Point2D to convert</param>
+        /// <returns>SwinGame Point2D</returns>
 		public static implicit operator SwinGameSDK.Point2D(PantMerchant.Point2D p){
-		// Automatically convert between my Point2D and SwinGame's Point2D
-			SwinGameSDK.Point2D result = new SwinGameSDK.Point2D ();
-			result.X = p.X;
-			result.Y = p.Y;
-			return result;
+			return new SwinGameSDK.Point2D() { X = p.X, Y = p.Y };
 		}
 
+        /// <summary>
+        /// Performs implicit conversion between PantMerchant's Point2D and 
+        /// SwinGame's in-built Point2D class.
+        /// </summary>
+        /// <param name="p">SwinGame Point2D to convert</param>
+        /// <returns>PantMerchant Point2D</returns>
 		public static implicit operator PantMerchant.Point2D(SwinGameSDK.Point2D p){
-            // Automatically convert between my Point2D and SwinGame's Point2D
-            PantMerchant.Point2D result = new PantMerchant.Point2D ();
-			result.X = Point2D.Round(p.X);
-			result.Y = Point2D.Round(p.Y);
-			return result;
+			return new PantMerchant.Point2D(Point2D.Round(p.X), Point2D.Round(p.Y));
 		}
 
         /// <summary>

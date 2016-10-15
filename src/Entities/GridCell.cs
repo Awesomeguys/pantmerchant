d561b52a64bc7a59a6a8d0ed3d1b1b276860249d
Daniel <forsyth.daniel@gmail.com>
2016-10-15 17:10:23 +1100

    Add GridCell class for determining properties of cells on grid

---
+++ src/Entities/GridCell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using SwinGameSDK;
+using System.Collections.Generic;
+using static SwinGameSDK.SwinGame;
+
+// Pantmerchant namespaces
+using PantMerchant.Controllers;
+
+namespace PantMerchant.Entities
+{
+    class GridCell
+    {
+        public BaseEntity Entity { get; private set; }
+
+        public Point2D Pos { get; }
+        public GridCell NeighbourTop {
+            get
+            {
+                return GameController.Instance.GetGrid(new Point2D(this.Pos.X, this.Pos.Y + 1));
+            }
+        }
+        public GridCell NeighbourRight
+        {
+            get
+            {
+                return GameController.Instance.GetGrid(new Point2D(this.Pos.X + 1, this.Pos.Y));
+            }
+        }
+        public GridCell NeighbourBottom
+        {
+            get
+            {
+                return GameController.Instance.GetGrid(new Point2D(this.Pos.X, this.Pos.Y - 1));
+            }
+        }
+        public GridCell NeighbourLeft
+        {
+            get
+            {
+                return GameController.Instance.GetGrid(new Point2D(this.Pos.X - 1, this.Pos.Y));
+            }
+        }
+
+        internal GridCell() : this(Point2D.Zero) { }
+        internal GridCell(Point2D p)
+        {
+            if (p == Point2D.Zero || GameController.Instance.GetGrid(p) != null)
+            {
+                this.Pos = p;
+            }
+            else
+            {
+                throw new GridPositionTakenExcepion();
+            }
+        }
+        private class GridPositionTakenExcepion : Exception
+        {
+            public GridPositionTakenExcepion() : base("There is already a grid with this position.") { }
+        }
+    }
+}

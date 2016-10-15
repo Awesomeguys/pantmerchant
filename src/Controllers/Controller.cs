821e85f46535942aff04f9e38cc0b7e0d050c42c
Daniel <forsyth.daniel@gmail.com>
2016-10-15 13:13:47 +1100

    Added documentation

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -7,20 +7,47 @@ namespace PantMerchant.Controllers
 {
     abstract class Controller
     {
+        /// <summary>
+        /// List of all UI elements instantiated. Added to whichever 
+        /// controller is currently in charge
+        /// </summary>
         public List<UIElement> UIElementList { get; }
+
+        /// <summary>
+        /// List of all IClickables instantiated. Added to whichever 
+        /// controller is currently in charge
+        /// </summary>
         public List<IClickable> IClickableList { get; }
+
+        /// <summary>
+        /// List of all IDrawables instantiated. Added to whichever 
+        /// controller is currently in charge
+        /// </summary>
         public List<IDrawable> IDrawableList { get; }
+
+        /// <summary>
+        /// The singleton instance of the current controller.
+        /// </summary>
         public static Controller Instance { get; }
 
+        /// <summary>
+        /// Initialises the base Controller class.
+        /// </summary>
         protected Controller()
         {
             UIElementList = new List<UIElement>();
             IClickableList = new List<IClickable>();
             IDrawableList = new List<IDrawable>();
         }
+
+
+        /// <summary>
+        /// Handles click actions for all IClickables being 
+        /// managed by the current controller
+        /// </summary>
         public void DoClickActions()
         {
-            foreach (IClickable c in StateController.IClickableList)
+            foreach (IClickable c in this.IClickableList)
             {
                 if (c.IsClicked())
                 {

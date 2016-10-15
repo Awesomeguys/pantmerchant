5dd24d90f5c19a594648239ae1aa316404c8b130
Daniel <forsyth.daniel@gmail.com>
2016-10-15 13:28:07 +1100

    Added GameController class

---
+++ src/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SwinGameSDK;
+using static SwinGameSDK.SwinGame;
+using System.Collections.Generic;
+
+namespace PantMerchant.Controllers
+{
+    class GameController : Controller
+    {
+        private static GameController _instance;
+
+        /// <summary>
+        /// The singleton instance of the current controller.
+        /// </summary>
+        public static new GameController Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameController();
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Static constructor for the current controller
+        /// </summary>
+        static GameController()
+        {
+            // Stuff goes here
+        }
+
+        public override void DoControllerStuff()
+        {
+            if (SwinGame.KeyTyped(KeyCode.EscapeKey))
+            {
+                StateController.PauseGame();
+            }
+            this.DoClickActions();
+            View.Draw();
+        }
+    }
+}

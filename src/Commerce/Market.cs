16aec6484f75a6f0e197dc626e0693da6d7f2c0d
David J Holland <stooge213@hotmail.com>
2016-10-15 16:04:06 +1100

    Add market

---
+++ src/Commerce/Market.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace PantMerchant
+{
+    /// <summary>
+    /// Static class that handles the market system.
+    /// </summary>
+    public static class Market
+    {
+        public static int RegularRetailPrice { get; private set; }
+        public static int StockPrice { get; private set; }
+        public static int StorePrice { get; set; }
+
+        static Market()
+        {
+            RegularRetailPrice = 50;
+            StockPrice = 20;
+            StorePrice = RegularRetailPrice;
+        }
+    }
+}

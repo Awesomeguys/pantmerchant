98064d83f738956cbb7723b9934f4e28ea5aca3c
Daniel <Daniel@TOPAXI>
2016-10-16 23:41:37 +1100

    Add Grid initialisation unit tests

---
+++ UnitTests/src/Entities/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pantmerchant namespace
+using PantMerchant;
+
+// Unit tests
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class BaseEntityTests
+    {
+        [Test]
+        public void GridStart()
+        {
+            StateController.StartGame();
+            TestEntity test = new TestEntity("UnitTestEntity", Point2D.Origin);
+
+            // Entity starts on the correct grid
+            Assert.IsTrue(GridCell.GetGrid(test.Position) == GridCell.Origin, "Entity does not start on the correct grid");
+            Assert.IsTrue(GridCell.GetGrid(test.Position).Entity == test, "Grid does not contain correct entity upon initialisation");
+        }
+    }
+}

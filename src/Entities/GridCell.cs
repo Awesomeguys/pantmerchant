4280909b1ca826425290268f8b897f888f3cec61
Daniel <Daniel@TOPAXI>
2016-10-16 19:37:03 +1100

    Add documentation

--- src/Entities/GridCell.cs
+++ src/Entities/GridCell.cs
@@ -5,17 +5,36 @@ using static SwinGameSDK.SwinGame;
 
 namespace PantMerchant
 {
+    /// <summary>
+    /// Basic in-game positional unit. Manages the entity 
+    /// which in contains, as well as references to it's
+    /// neighbours.
+    /// </summary>
     public class GridCell : IDrawable
     {
+        /// <summary>
+        /// The entity which occupies this GridCell
+        /// </summary>
         public BaseEntity Entity { get; private set; }
 
+        /// <summary>
+        /// Position of the GridCell
+        /// </summary>
         public Point2D Coordinate { get; }
+
+        /// <summary>
+        /// The GridCell to the top (top-right) of this one.
+        /// </summary>
         public GridCell NeighbourTop {
             get
             {
                 return GameController.Instance.GetGrid(new Point2D(this.Coordinate.X, this.Coordinate.Y + 1));
             }
         }
+
+        /// <summary>
+        /// The GridCell to the right (bottom-right) of this one.
+        /// </summary>
         public GridCell NeighbourRight
         {
             get
@@ -23,6 +42,10 @@ namespace PantMerchant
                 return GameController.Instance.GetGrid(new Point2D(this.Coordinate.X + 1, this.Coordinate.Y));
             }
         }
+
+        /// <summary>
+        /// The GridCell to the bottom (bottom-left) of this one.
+        /// </summary>
         public GridCell NeighbourBottom
         {
             get
@@ -30,6 +53,10 @@ namespace PantMerchant
                 return GameController.Instance.GetGrid(new Point2D(this.Coordinate.X, this.Coordinate.Y - 1));
             }
         }
+
+        /// <summary>
+        /// The GridCell to the left (top-left) of this one.
+        /// </summary>
         public GridCell NeighbourLeft
         {
             get
@@ -38,7 +65,17 @@ namespace PantMerchant
             }
         }
 
+        /// <summary>
+        /// Initialises a new GridCell instance 
+        /// with the position set to the origin.
+        /// </summary>
         internal GridCell() : this(Point2D.Origin) { }
+
+        /// <summary>
+        /// Initialises a new GridCell instance
+        /// with the position set to p
+        /// </summary>
+        /// <param name="p">The position of the Gridcell</param>
         internal GridCell(Point2D p)
         {
             this.Coordinate = p;
@@ -48,6 +85,9 @@ namespace PantMerchant
         //    public GridPositionTakenExcepion() : base("There is already a grid with this position.") { }
         //}
 
+        /// <summary>
+        /// Highlights the GridCell by drawing a black line around the edges.
+        /// </summary>
         public void Draw()
         {
             Point2D screenMiddle = new Point2D(ScreenWidth()/2, ScreenHeight()/2);

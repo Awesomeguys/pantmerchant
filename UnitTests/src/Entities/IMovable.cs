7e1e36e311dd46b95efc222d851a957322b4c02f
Daniel <Daniel@TOPAXI>
2016-10-16 23:42:18 +1100

    Add tests for IMovable methods

---
+++ UnitTests/src/Entities/IMovable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pantmerchant namespace
+using PantMerchant;
+
+// Unit tests
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class IMovableTests
+    {
+        [Test]
+        public void Move()
+        {
+            StateController.StartGame();
+            TestEntity test;
+
+            GridCell.Origin.Entity = null;
+            test = new TestEntity("UnitTestEntity", Point2D.Origin);
+            
+            // Moving sets the grid to the correct neighbour
+            test.Move(Direction.Up);
+            Assert.IsTrue(GridCell.GetGrid(test.Position) == GridCell.Origin.NeighbourTop, "Entity does not move to the correct grid");
+            Assert.IsTrue(GridCell.Origin.NeighbourTop.Entity == test, "Grid does not contain correct entity after move");
+
+            GridCell.Origin.Entity = null;
+            test = new TestEntity("UnitTestEntity", Point2D.Origin);
+
+            // Moving sets the grid to the correct neighbour
+            test.Move(Direction.Right);
+            Assert.IsTrue(GridCell.GetGrid(test.Position) == GridCell.Origin.NeighbourRight, "Entity does not move to the correct grid");
+            Assert.IsTrue(GridCell.Origin.NeighbourRight.Entity == test, "Grid does not contain correct entity after move");
+
+            GridCell.Origin.Entity = null;
+            test = new TestEntity("UnitTestEntity", Point2D.Origin);
+
+            // Moving sets the grid to the correct neighbour
+            test.Move(Direction.Down);
+            Assert.IsTrue(GridCell.GetGrid(test.Position) == GridCell.Origin.NeighbourBottom, "Entity does not move to the correct grid");
+            Assert.IsTrue(GridCell.Origin.NeighbourBottom.Entity == test, "Grid does not contain correct entity after move");
+
+            GridCell.Origin.Entity = null;
+            test = new TestEntity("UnitTestEntity", Point2D.Origin);
+
+            // Moving sets the grid to the correct neighbour
+            test.Move(Direction.Left);
+            Assert.IsTrue(GridCell.GetGrid(test.Position) == GridCell.Origin.NeighbourLeft, "Entity does not move to the correct grid");
+            Assert.IsTrue(GridCell.Origin.NeighbourLeft.Entity == test, "Grid does not contain correct entity after move");
+        }
+    }
+}

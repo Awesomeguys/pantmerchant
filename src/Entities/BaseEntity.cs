922f4d9aba27700325ec48b8279e96983a859e88
Daniel <Daniel@TOPAXI>
2016-10-17 13:43:46 +1100

    Add Facing property
    This keeps track of which direction the entity is facing. Used to determine which art asset to draw to the screen.

--- src/Entities/BaseEntity.cs
+++ src/Entities/BaseEntity.cs
@@ -31,12 +31,14 @@ namespace PantMerchant
                     GridCell.GetGrid(this.Position).ScreenPos;
             }
         }
-
         /// <summary>
         /// The position on the grid.
         /// </summary>
         public Point2D Position { get; set; }
-
+        /// <summary>
+        /// The direction the entity is facing.
+        /// </summary>
+        public Direction Facing { get; protected set; }
         /// <summary>
         /// A list of grid points relative to the
         /// position that the entity will take up.

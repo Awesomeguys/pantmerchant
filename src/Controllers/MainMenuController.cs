19a400a2dd0790397dfb2c4ece1e31af38c01680
frostypls <forsyth.daniel@gmail.com>
2016-10-19 18:26:14 +1100

    Add a bunch of stuff
    1. Make _Music property non-static
    2. Throw exception if Controller already there
    3. Add settings menu

--- src/Controllers/MainMenuController.cs
+++ src/Controllers/MainMenuController.cs
@@ -15,7 +15,8 @@ namespace PantMerchant
         /// <summary>
         /// The singleton instance of the current controller.
         /// </summary>
-        public static new MainMenuController Instance {
+        public static new MainMenuController Instance
+        {
             get
             {
                 if (_instance == null)
@@ -25,8 +26,11 @@ namespace PantMerchant
                 return _instance;
             }
         }
-        
-        private static Music _Music;
+
+        /// <summary>
+        /// The music to play on the main menu
+        /// </summary>
+        private Music _Music;
 
         /// <summary>
         /// Static constructor for the current controller
@@ -34,13 +38,31 @@ namespace PantMerchant
         static MainMenuController()
         {
             MainMenuController.BuildMainMenu();
-            _Music = GameResources.GameMusic("MainMenu");
+            MainMenuController.BuildSettingsMenu();
         }
 
         /// <summary>
-        /// The main menu to draw.
+        /// Initialises a new instance of the MainMenuController class
+        /// </summary>
+        private MainMenuController()
+        {
+            if (MainMenuController._instance != null)
+            {
+                throw new ControllerAlreadyInitialisedException();
+            }
+            this._Music = GameResources.GameMusic("MainMenu");
+        }
+
+
+        /// <summary>
+        /// The main menu
         /// </summary>
         public UIContainer MainMenu { get; private set; }
+
+        /// <summary>
+        /// The settings menu
+        /// </summary>
+        public UIContainer Settings { get; private set; }
 
         /// <summary>
         /// Handles click actions for all IClickables being 
@@ -68,8 +90,7 @@ namespace PantMerchant
         /// </summary>
         private static void BuildMainMenu()
         {
-            // Create some UIs
-            UIContainer Menu = new UIContainer(
+            Instance.MainMenu = new UIContainer(
                     "Main Menu",
                     Point2D.ScreenMiddle - new Point2D(100, 100),
                     new Point2D(200, 200),
@@ -82,18 +103,72 @@ namespace PantMerchant
                     "New Game",
                     new Action(() => { StateController.Instance.StartGame(); }),
                     "New Game",
-                    Menu
+                    Instance.MainMenu
                 )
             );
+
+            MenuList.Add(
+                new MenuElement(
+                    "Settings",
+                    new Action(() => { MainMenuController.Instance.OpenSettingsMenu(); }),
+                    "Settings",
+                    Instance.MainMenu
+                )
+            );
+
 
             MenuList.Add(
                 new MenuElement(
                     "Quit",
                     new Action(() => { StateController.Instance.QuitToDesktop(); }),
                     "Quit",
-                    Menu
+                    Instance.MainMenu
                 )
             );
+        }
+
+        /// <summary>
+        /// Initialises the settings menu.
+        /// </summary>
+        private static void BuildSettingsMenu()
+        {
+            // Create some UIs
+            Instance.Settings = new UIContainer(
+                    "Settings",
+                    Point2D.ScreenMiddle - new Point2D(100, 100),
+                    new Point2D(200, 200),
+                    MenuType.Auto
+            );
+
+            Instance.Settings.Hide();
+
+            List<UIElement> MenuList = new List<UIElement>();
+            MenuList.Add(
+                new MenuElement(
+                    "Back to Main Menu",
+                    new Action(() => { if (MusicPlaying()) { PauseMusic(); } else { ResumeMusic(); } }),
+                    "Back to Main Menu",
+                    Instance.Settings
+                )
+            );
+
+            MenuList.Add(
+                new MenuElement(
+                    "Toggle Sound",
+                    new Action(() => { if (MusicPlaying()) { PauseMusic(); } else { ResumeMusic(); } }),
+                    "Toggle Sound",
+                    Instance.Settings
+                )
+            );
+        }
+
+        private void OpenSettingsMenu()
+        {
+            if (!this.MainMenu.IsHidden)
+            {
+                this.MainMenu.Hide();
+                this.Settings.Show();
+            }
         }
     }
 }

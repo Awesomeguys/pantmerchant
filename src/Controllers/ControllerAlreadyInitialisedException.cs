4ccd79b6b004f94817a990a4310f3985af8e5d14
frostypls <forsyth.daniel@gmail.com>
2016-10-19 18:24:09 +1100

    Add new exception

---
+++ src/Controllers/ControllerAlreadyInitialisedException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PantMerchant
+{
+    /// <summary>
+    /// Thrown if the controller is already instantiated
+    /// </summary>
+    [Serializable]
+    internal class ControllerAlreadyInitialisedException : Exception
+    {
+        public ControllerAlreadyInitialisedException()
+            : this("The controller has already been instantiated. Something has gone horribly wrong somewhere.")
+        {
+        }
+
+        public ControllerAlreadyInitialisedException(string message) : base(message)
+        {
+        }
+
+        public ControllerAlreadyInitialisedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ControllerAlreadyInitialisedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
